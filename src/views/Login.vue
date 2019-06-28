376f198100061e8b373a702c61a2b545fa6fe518
liangzhifang <595945685@qq.com>
2019-06-28 12:02:10 +0800

    登录实现，跳转到home页面

---
+++ src/views/Login.vue
@@ -0,0 +1,110 @@
+<template>
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form
+        ref="loginForm"
+        :rules="formRules"
+        :model="form"
+        class="login-form"
+        label-width="80px"
+        label-position="top"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      form: {
+        username: "admin",
+        password: "123456"
+      },
+      formRules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          },
+          {
+            min: 5,
+            max: 12,
+            message: "长度在5-12个字符",
+            trigger: "change"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在6-15个字符",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios({
+            url: "http://localhost:8888/api/private/v1/login",
+            method: "post",
+            data: this.form
+          }).then(({ data: { data, meta } }) => {
+            // console.log(data, meta);
+            if (meta.status === 200) {
+              this.$router.push("./home");
+            }
+          });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.row-bg {
+  height: 100%;
+  background-color: #2d434c;
+}
+
+.login-form {
+  background-color: #fff;
+  padding: 30px 20px;
+  border-radius: 10px;
+  min-width: 350px;
+}
+</style>
